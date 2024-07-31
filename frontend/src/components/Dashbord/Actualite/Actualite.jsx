a16595b808cb763d32574d5c9c82d7299c72023e
Safa Ben Othmen <124161411+Safabenothmen@users.noreply.github.com>
2024-07-31 17:38:07 +0200

    Add files via upload

---
+++ frontend/src/components/Dashbord/Actualite/Actualite.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Dashbord from '../Dashbord'
+
+
+function Actualite (props) {
+  return (
+      <>
+        <div className='wrapper' >
+          <h1 className='page-title'>Actualite</h1>
+        </div >
+      </>
+    )
+}
+
+export default Actualite
