bba269fc62ae3044110c2b81f8dc7a92cde02a79
Safa Ben Othmen <124161411+Safabenothmen@users.noreply.github.com>
2024-07-31 18:07:45 +0200

    Add files via upload

---
+++ frontend/src/components/UI/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './header.css'
+
+import Nav from '../Navbar/Navbar';
+
+const header = () => {
+  return (
+    <div className='header'>
+      
+      <Nav />
+    </div>
+  );
+};
+
+export default header;
+  
