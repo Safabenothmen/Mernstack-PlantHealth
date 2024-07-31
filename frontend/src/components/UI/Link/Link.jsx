95db788336b8a7a7e3c6e4ef2c429c5ca0f1badc
Safa Ben Othmen <124161411+Safabenothmen@users.noreply.github.com>
2024-07-31 18:08:35 +0200

    Add files via upload

---
+++ frontend/src/components/UI/Link/Link.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './link.css';
+import { Outlet } from 'react-router-dom'
+
+const link = () => {
+  return (
+      <div >
+
+            
+            
+
+        </div>
+
+  );
+};
+
+export default link;
