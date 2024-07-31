7c2546809fc227db373a87e042a43a42aea85846
Safa Ben Othmen <124161411+Safabenothmen@users.noreply.github.com>
2024-07-31 17:51:13 +0200

    Add files via upload

---
+++ frontend/src/components/Dashbord/ActualiteExpert/Actualiteexpert.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Dashbord from '../Dashbord'
+
+
+function Actualiteexpert (props) {
+  return (
+      <>
+        <div className='wrapper' >
+          <h1 className='page-title'>Actualite expert</h1>
+        </div >
+      </>
+    )
+}
+
+export default Actualiteexpert
