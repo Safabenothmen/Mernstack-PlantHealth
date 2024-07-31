5ec0fab17ceaad36acfc915eb7bf41d0743aa935
Safa Ben Othmen <124161411+Safabenothmen@users.noreply.github.com>
2024-07-31 17:39:29 +0200

    Add files via upload

---
+++ frontend/src/components/Dashbord/ActualiteAdmin/Actualiteadmin.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Dashbord from '../Dashbord'
+
+
+function Actualiteadmin (props) {
+  return (
+      <>
+        <div className='wrapper' >
+          <h1 className='page-title'>Actualite admin</h1>
+        </div >
+      </>
+    )
+}
+
+export default Actualiteadmin
