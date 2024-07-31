ff901f4a0f0f85b0c3afe9bc17f445ee1660e43c
Safa Ben Othmen <124161411+Safabenothmen@users.noreply.github.com>
2024-07-31 17:40:39 +0200

    Add files via upload

---
+++ frontend/src/components/Dashbord/ActualiteAgri/Actulaiteagri.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Dashbord from '../Dashbord'
+
+
+function Actualiteagri (props) {
+  return (
+      <>
+        <div className='wrapper' >
+          <h1 className='page-title'>Actualite agriculteur</h1>
+        </div >
+      </>
+    )
+}
+
+export default Actualiteagri
